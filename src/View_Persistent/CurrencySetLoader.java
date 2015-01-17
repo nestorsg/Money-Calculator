7da03eaa2c14cf5d767bdef761dce81ee92835f0
Néstor <Nstor@10.230.26.136>
2015-01-17 16:38:30 +0000

    Terminado con dos errores en Application

--- src/View_Persistent/CurrencySetLoader.java
+++ src/View_Persistent/CurrencySetLoader.java
@@ -14,5 +14,9 @@ public class CurrencySetLoader {
     public void load() {
         currencySet.add(new Currency("EUR", "Euro", "€"));
         currencySet.add(new Currency("USD", "Dolar", "$"));
+        currencySet.add(new Currency("GBP", "Great Britain Pound", "£"));
+        currencySet.add(new Currency("CAD", "Canadian Dollar", "C$"));
+        currencySet.add(new Currency("CNY", "Chinese Yuan", "¥"));
+        currencySet.add(new Currency("JPY", "Japanese Yen", "¥"));
     }
 }
--- src/View_Persistent/ExchangeRateLoader.java
+++ src/View_Persistent/ExchangeRateLoader.java
@@ -1,7 +1,48 @@
 package View_Persistent;
 
+import Model.Currency;
+import Model.ExchangeRate;
+import java.util.ArrayList;
+
 public class ExchangeRateLoader {
 
-    public void load() {
+    ArrayList<String> currency;
+    ArrayList<Double> rate;
+    
+    
+    public ExchangeRate load(Currency from, Currency to) {
+        setExchangeRate();
+        return new ExchangeRate(from,to,calculateRate(from,to));
     }
+
+    private void setExchangeRate() {
+        addCurrency();
+        addRate();
+    }
+
+    private void addCurrency() {
+        currency = new ArrayList<>();
+        currency.add("CAD");
+        currency.add("CNY");
+        currency.add("EUR");
+        currency.add("GBP");
+        currency.add("JPY");
+        currency.add("USD");
+    }
+
+    private void addRate() {
+        rate = new ArrayList<>();
+        rate.add(1.42159181);
+        rate.add(7.5);
+        rate.add(1.0);
+        rate.add(0.79610815);
+        rate.add(147.0);
+        rate.add(1.244325);
+        
+    }
+
+    public double calculateRate(Currency from, Currency to) {
+        return (1/rate.get(currency.indexOf(from.getCode()))) * rate.get(currency.indexOf(to.getCode()));
+    }
+    
 }
